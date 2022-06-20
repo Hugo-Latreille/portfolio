649cad1ca6da3af3bc219e243662de6f03f43b6d
Hugo Latreille <hugo.latreille@gmail.com>
2022-06-20 09:28:44 +0200

    :zap:feat: add snap scroll

--- client/src/PortfolioContainer/Toggle/Toggle.js
+++ client/src/PortfolioContainer/Toggle/Toggle.js
@@ -6,18 +6,21 @@ import { ThemeContext } from "./../../context";
 
 const Toggle = () => {
 	const theme = useContext(ThemeContext);
+	const darkMode = theme.state.darkMode;
 	const handleClick = () => {
 		theme.dispatch({ type: "TOGGLE" });
 	};
 
 	return (
-		<div className="toggle">
+		<div className="toggle" onClick={handleClick}>
 			<img src={Sun} alt="" className="toggle__img" />
 			<img src={Moon} alt="" className="toggle__img" />
 			<div
 				className="toggle__button"
-				onClick={handleClick}
-				style={{ left: theme.state.darkMode ? "0" : "25px" }}
+				style={{
+					left: theme.state.darkMode ? "0" : "25px",
+					backgroundColor: darkMode ? "rgba(25, 138, 153, 1)" : "#26233a",
+				}}
 			></div>
 		</div>
 	);
