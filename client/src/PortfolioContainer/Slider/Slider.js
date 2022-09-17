6d4674871026ae09195d5ca3bd3102b93bdbf197
Hugo Latreille <hugo.latreille@gmail.com>
2022-09-17 14:37:07 +0200

    :zap:feat: replace handmade slider by swiper.js

--- client/src/PortfolioContainer/Slider/Slider.js
+++ client/src/PortfolioContainer/Slider/Slider.js
@@ -3,56 +3,67 @@ import { useContext, useState } from "react";
 import { MdOutlineArrowBackIosNew, MdArrowForwardIos } from "react-icons/md";
 import ScreenHeading from "../../utilities/ScreenHeading/ScreenHeading";
 import { ThemeContext } from "../../context";
+//SWIPER
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination, EffectCoverflow } from "swiper";
+import "swiper/css";
+import "swiper/css/free-mode";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
 
 const Slider = ({ forwardRef, isVisible3 }) => {
 	const theme = useContext(ThemeContext);
 	const darkMode = theme.state.darkMode;
 	const [currentSlide, setCurrentSlide] = useState(0);
+
+	//? Régler slider à 4, composant modale avec prop useState true, modale absolute on click élément develott, gérer mobile
+
 	const data = [
 		{
 			id: "1",
+			title: "Develott",
+			desc: (
+				<p>
+					Develott est une plateforme de mise en relation, d'échange et de
+					collaboration entre développeurs juniors autour de projets
+					open-sources proposés par la communauté. <br /> Front-End :
+					Javascript, React, Redux (Redux Toolkit / RTK Query), Sass <br />{" "}
+					Back-End : Javascript, NodeJs, Express, PostgreSql, Swagger, Sqitch,
+					JWT, NodeMailer, Passport.js{" "}
+				</p>
+			),
+			img: "assets/develott.png",
+			// liveLink: "https://hugo-latreille.github.io/",
+			video: "https://tinyurl.com/develott",
+		},
+		{
+			id: "2",
 			icon: "/assets/github.svg",
-			title: "Test",
-			desc: "test",
-			// img: "assets/portfolio.gif",
-			video: `<iframe width="560" height="315" src="https://www.youtube.com/embed/PNj_SqhSVg8?start=5153" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>`,
+			title: "Chat React / socket.io",
+			desc: "App fullstack de chat React/Socket.io/MongoDB",
+			img: "assets/chatApp.gif",
+			link: "https://github.com/Hugo-Latreille/ReactSocketMongoChat",
+			liveLink: "https://reactsocketchat.herokuapp.com/",
+		},
+		{
+			id: "3",
+			icon: "/assets/github.svg",
+			title: "VanillaJs SUTOM",
+			desc: "Une copie du SUTOM/Wordle en vanilla JS",
+			img: "assets/sutom.gif",
+			link: "https://github.com/Hugo-Latreille/hugo-latreille.github.io/tree/master/sutom",
+			liveLink: "https://hugo-latreille.github.io/sutom/",
+		},
+		{
+			id: "4",
+			icon: "/assets/github.svg",
+			title: "Porfolio CSS",
+			desc: "Un premier essai de porfolio en pur HTML/CSS après une semaine de formation",
+			img: "assets/portfolio.gif",
 			link: "https://github.com/Hugo-Latreille/hugo-latreille.github.io",
 			liveLink: "https://hugo-latreille.github.io/",
 		},
-		// {
-		// 	id: "2",
-		// 	icon: "/assets/github.svg",
-		// 	title: "Porfolio CSS",
-		// 	desc: "Un premier essai de porfolio en pur HTML/CSS après une semaine de formation",
-		// 	img: "assets/portfolio.gif",
-		// 	link: "https://github.com/Hugo-Latreille/hugo-latreille.github.io",
-		// 	liveLink: "https://hugo-latreille.github.io/",
-		// },
-		// {
-		// 	id: "3",
-		// 	icon: "/assets/github.svg",
-		// 	title: "Chat React / socket.io",
-		// 	desc: "App fullstack de chat React/Socket.io/MongoDB",
-		// 	img: "assets/chatApp.gif",
-		// 	link: "https://github.com/Hugo-Latreille/ReactSocketMongoChat",
-		// 	liveLink: "https://reactsocketchat.herokuapp.com/",
-		// },
-		// {
-		// 	id: "4",
-		// 	icon: "/assets/github.svg",
-		// 	title: "VanillaJs SUTOM",
-		// 	desc: "Une copie du SUTOM/Wordle en vanilla JS",
-		// 	img: "assets/sutom.gif",
-		// 	link: "https://github.com/Hugo-Latreille/hugo-latreille.github.io/tree/master/sutom",
-		// 	liveLink: "https://hugo-latreille.github.io/sutom/",
-		// },
 	];
-
-	const handleClick = (way) => {
-		way === "left"
-			? setCurrentSlide(currentSlide > 0 ? currentSlide - 1 : 2)
-			: setCurrentSlide(currentSlide < data.length - 1 ? currentSlide + 1 : 0);
-	};
 
 	return (
 		<div
@@ -72,12 +83,38 @@ const Slider = ({ forwardRef, isVisible3 }) => {
 					subHeading={"Réalisés en 2022"}
 					title={"Quelques projets"}
 				/>
-				<div
+
+				<Swiper
+					breakpoints={{
+						// when window width is >= 300px
+						300: {
+							width: 300,
+							slidesPerView: 1,
+						},
+						// when window width is >= 760px
+						760: {
+							width: 760,
+							slidesPerView: 1,
+						},
+						// when window width is >= 900px
+						900: {
+							width: 800,
+							slidesPerView: 1,
+						},
+					}}
+					spaceBetween={30}
+					freeMode={true}
+					grabCursor={true}
+					// navigation
+					pagination={{
+						clickable: true,
+					}}
+					modules={[Pagination, Navigation, EffectCoverflow]}
+					effect={"coverflow"}
 					className="slider"
-					style={{ transform: `translateX(-${currentSlide * 3150}px)` }}
 				>
 					{data.map((d) => (
-						<div className="container" key={d.id}>
+						<SwiperSlide className="container" key={d.id}>
 							<div className="item">
 								<div className="browser">
 									<div className="browser-circle"></div>
@@ -86,33 +123,53 @@ const Slider = ({ forwardRef, isVisible3 }) => {
 								</div>
 								<div className="left">
 									<div className="leftContainer">
-										<div className="imgContainer">
-											<a href={d.link} target="_blank" rel="noreferrer">
-												<img src={d.icon} alt="" />
-											</a>
-										</div>
+										{d.id !== "1" && (
+											<div className="imgContainer">
+												<a href={d.link} target="_blank" rel="noreferrer">
+													<img src={d.icon} alt="" />
+												</a>
+											</div>
+										)}
+
 										<h2>{d.title}</h2>
 										<p>{d.desc}</p>
-										<a href={d.liveLink} target="_blank" rel="noreferrer">
-											<span>Demo</span>
-										</a>
+										{d.id !== "1" && (
+											<a href={d.liveLink} target="_blank" rel="noreferrer">
+												<span>Demo</span>
+											</a>
+										)}
 									</div>
 								</div>
 								<div className="right">
-									<img src={d.img} alt="" />
+									{d.video ? (
+										<div className="video">
+											<iframe
+												width="560"
+												height="315"
+												src="https://www.youtube.com/embed/PNj_SqhSVg8?start=5153"
+												title="YouTube video player"
+												frameBorder="0"
+												allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+												allowFullScreen
+											></iframe>
+										</div>
+									) : (
+										<img src={d.img} alt="" />
+									)}
 								</div>
 							</div>
-						</div>
+						</SwiperSlide>
 					))}
-				</div>
-				<MdOutlineArrowBackIosNew
+				</Swiper>
+
+				{/* <MdOutlineArrowBackIosNew
 					className="arrow left"
 					onClick={() => handleClick("left")}
 				/>
 				<MdArrowForwardIos
 					className="arrow right"
 					onClick={() => handleClick()}
-				/>
+				/> */}
 			</div>
 		</div>
 	);
